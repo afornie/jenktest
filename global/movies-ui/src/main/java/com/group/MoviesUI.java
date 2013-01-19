8866e6faeb7e6b635b23f425c9e406621842216a
antonio.fornie <antonio.fornie@irdeto.com>
2013-01-19 18:19:40 +0100

    Basic structure of multimodular mvn project

---
+++ global/movies-ui/src/main/java/com/group/MoviesUI.java
@@ -0,0 +1,8 @@
+package com.group;
+
+public class MoviesUI{
+	
+    public void displayTitle(){
+	    System.out.println("MyMovie");
+	}
+}
---
+++ global/movies/src/main/java/com/group/Movies.java
@@ -0,0 +1,9 @@
+package com.group;
+
+public class Movies{
+    public static final String TITLE = "Ghost in the Shell";
+	
+    public String getTitle(){
+	    return TITLE;
+	}
+}
